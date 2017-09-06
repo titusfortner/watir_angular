a2309d2fda3c18e6cf3ecc926818cebf653eb11b
Titus Fortner <titusfortner@gmail.com>
2017-09-05 18:21:48 -0700

    add guards

--- spec/watirspec_helper.rb
+++ spec/watirspec_helper.rb
@@ -8,6 +8,13 @@ WatirSpec.implementation do |watirspec|
   watirspec.name = :watir_angular
   watirspec.browser_class = Watir::Browser
   watirspec.browser_args = [:chrome, opts]
+
+  watirspec.guard_proc = lambda do |watirspec_guards|
+    watir_angular_guards = [:chrome]
+    watir_angular_guards << :watir_angular
+
+    watirspec_guards.any? { |guard| watir_angular_guards.include?(guard) }
+  end
 end
 
 WatirSpec.run!
